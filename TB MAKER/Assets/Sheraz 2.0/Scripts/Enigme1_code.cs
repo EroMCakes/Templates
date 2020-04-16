c87fdeb2430ec6df629e5325d76a5f5951dcb05e
EroMCakes <44838692+EroMCakes@users.noreply.github.com>
2020-04-16 11:39:19 +0100

    comitting project

---
+++ TB MAKER/Assets/Sheraz 2.0/Scripts/Enigme1_code.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enigme1_code : MonoBehaviour
+{
+
+    public InputField Pass;
+    public string code = null;
+    public Text txt;
+    public string To_Load;
+
+
+    public void Enter_Push()
+    {
+        code = Pass.text;
+        if (Pass.text != null)
+        {
+            if (code == "1735")
+            {
+                txt.text = "Code valide !";
+                UnityEngine.SceneManagement.SceneManager.LoadScene(To_Load);
+            }
+            else if(code == "5384")
+            {
+                txt.text = "Code valide !";
+                UnityEngine.SceneManagement.SceneManager.LoadScene(To_Load);
+            }
+            else if (code == "586")
+            {
+                txt.text = "Code valide !";
+                UnityEngine.SceneManagement.SceneManager.LoadScene(To_Load);
+            }
+            else if (code == "369")
+            {
+                txt.text = "Code valide !";
+                UnityEngine.SceneManagement.SceneManager.LoadScene(To_Load);
+            }
+            else if (code == "319")
+            {
+                txt.text = "Code valide !";
+                UnityEngine.SceneManagement.SceneManager.LoadScene(To_Load);
+            }
+            else if (code == "132")
+            {
+                txt.text = "Code valide !";
+                UnityEngine.SceneManagement.SceneManager.LoadScene(To_Load);
+            }
+            else if (code == "PLC" || code == "plc")
+            {
+                txt.text = "Code valide !";
+                UnityEngine.SceneManagement.SceneManager.LoadScene(To_Load);
+            }
+            else
+            {
+                txt.text = "Code erroné !";
+                Pass.text = null;
+            }
+        }
+    }
+}
